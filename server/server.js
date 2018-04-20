e060d05c06542c0f03f167429df8fd6a94fbd3fa
Carlos Alonso Turner Benites <carlosturnerbenites@gmail.com>
2018-04-19 20:44:52 -0500

    end

--- server/server.js
+++ server/server.js
@@ -1,20 +1,22 @@
 'use strict';
 
-const Promise = require('bluebird');
+// import { constants } from 'fs';
+
+
+// const Promise = require('bluebird');
 // custom logger
 const log = require('./logger.js');
 const express = require('express');
 
 const mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost/test');
 const Schema = mongoose.Schema;
 
 
-var cassandra = require('cassandra-driver');
+const cassandra = require('cassandra-driver');
 // var async = require('async');
-var client = new cassandra.Client({ contactPoints: ['127.0.0.1'], keyspace: 'test' });
-
-
+const client = new cassandra.Client({ contactPoints: ['127.0.0.1'], keyspace: 'test' });
 
 const app = express();
 // app.set('view engine', 'html');
@@ -27,12 +29,8 @@ app.use(express.static(`${__dirname}/../public`));
 const port = process.env.PORT || 8000;
 const server = require('http').Server(app);
 
-// boilerplate version
-const version = `Express-Boilerplate v${require('../package.json').version}`;
-
 // start server
 server.listen(port, () => {
-  log.info(version);
   log.info(`Listening on port ${port}`);
 });
 
@@ -41,9 +39,11 @@ const bodyParser = require('body-parser');
 // create application/json parser
 const jsonParser = bodyParser.json();
 // create application/x-www-form-urlencoded parser
+/*
 const urlencodedParser = bodyParser.urlencoded({
   extended: false,
 });
+*/
 
 function handleError(err) {
   console.log(err);
@@ -53,7 +53,7 @@ const personSchema = Schema({
   _id: Schema.Types.ObjectId,
   name: String,
   age: Number,
-  stories: [{ type: Schema.Types.ObjectId, ref: 'Story' }]
+  stories: [{ type: Schema.Types.ObjectId, ref: 'Story' }],
 });
 
 const storySchema = Schema({
@@ -86,7 +86,7 @@ app.get('/api/stories', jsonParser, (req, res) => {
 
 app.post('/api/stories', jsonParser, (req, res) => {
   if (!req.body) return res.sendStatus(404);
-  let data = req.body;
+  const data = req.body;
 
   const story = new Story(data);
 
@@ -110,7 +110,7 @@ app.delete('/api/stories', jsonParser, (req, res) => {
 
 app.delete('/api/story', jsonParser, (req, res) => {
   if (!req.body) return res.sendStatus(404);
-  let data = req.body;
+  const data = req.body;
 
   Story.remove({ _id: data._id }, (err, story) => {
     if (err) return handleError(err);
@@ -126,50 +126,50 @@ app.delete('/api/story', jsonParser, (req, res) => {
 
 app.put('/api/story', jsonParser, (req, res) => {
   if (!req.body) return res.sendStatus(404);
-  let data = req.body;
+  const data = req.body;
 
-  Story.update({
-    _id: data._id,
-  },
-  {
-    $set: {
-      title: data.title,
-      tags: data.tags,
-      author: data.author._id,
+  Story.update(
+    {
+      _id: data._id,
     },
-  }, (err, story) => {
-    if (err) return handleError(err);
-    return res.json(story); // create user in req.body
-  });
+    {
+      $set: {
+        title: data.title,
+        tags: data.tags,
+        author: data.author._id,
+      },
+    }, (err, story) => {
+      if (err) return handleError(err);
+      return res.json(story); // create user in req.body
+    },
+  );
 });
 
 app.delete('/api/tag', jsonParser, (req, res) => {
   if (!req.body) return res.sendStatus(404);
-  let data = req.body;
+  const data = req.body;
 
-  Story.findOne({ tags: { $elemMatch: { $in: [data.name] } } }, (err, story) => {
-    if (err) return handleError(err);
+  Story.findOne({ tags: { $elemMatch: { $in: [data.name] } } }, (errFindOne, story) => {
+    if (errFindOne) return handleError(errFindOne);
     // return res.json(story); // create user in req.body
-    console.log(story)
     if (story === null) {
-      client.execute('DELETE FROM test.tags WHERE id = ?;', [data.id], (err, result) => {
-        if (err) return res.json(err);
+      client.execute('DELETE FROM test.tags WHERE id = ?;', [data.id], (errDelete, resultDelete) => {
+        if (errDelete) return res.json(errDelete);
         // thats it!
-        client.execute("SELECT * FROM tags", (err, result) => {
-          if (err) return handleError(err);
-          res.json(result.rows)
+        client.execute('SELECT * FROM tags', (errSelect, resultSelect) => {
+          if (errSelect) return handleError(errSelect);
+          return res.json(resultSelect.rows);
         });
       });
     } else {
       res.status(404).json({ message: 'No se puede borrar el tag' });
     }
   });
-
 });
 
 app.get('/api/tags', jsonParser, (req, res) => {
   if (!req.body) return res.sendStatus(404);
-  let data = req.body;
+  // const data = req.body;
 
   // cqlsh
   // DESCRIBE KEYSPACES
@@ -178,22 +178,22 @@ app.get('/api/tags', jsonParser, (req, res) => {
   // insert into test.tags (id, name) values(now(), 'bueno');
 
 
-  client.execute("SELECT * FROM tags", (err, result) => {
+  client.execute('SELECT * FROM tags', (err, result) => {
     if (err) return handleError(err);
-    res.json(result.rows)
+    return res.json(result.rows);
   });
 });
 
 app.post('/api/tags', jsonParser, (req, res) => {
   if (!req.body) return res.sendStatus(404);
-  let data = req.body;
+  const data = req.body;
 
-  client.execute('insert into test.tags (id, name) values(now(), ?);', [data.name], (err, result) => {
-    if (err) return res.json(err);
+  client.execute('insert into test.tags (id, name) values(now(), ?);', [data.name], (errInsert, result) => {
+    if (errInsert) return res.json(errInsert);
     // thats it!
-    client.execute("SELECT * FROM tags", (err, result) => {
-      if (err) return handleError(err);
-      res.json(result.rows)
+    client.execute('SELECT * FROM tags', (errSelect, result) => {
+      if (errSelect) return handleError(errSelect);
+      return res.json(result.rows);
     });
   });
 });
